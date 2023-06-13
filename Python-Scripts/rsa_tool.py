60c258640c74334c7e7a2300b4d087c40bf572f4
Francesco Lucarini (Apothiphis_Z) <69625915+FrancescoLucarini@users.noreply.github.com>
2023-06-13 21:22:00 +0200

    Update rsa_tool.py
    made it 'smarter' so more useful

--- Python-Scripts/rsa_tool.py
+++ Python-Scripts/rsa_tool.py
@@ -1,20 +1,24 @@
-from Crypto.Util.number import inverse, long_to_bytes
-import sys
 from factordb.factordb import FactorDB
+from Crypto.Util.number import inverse, long_to_bytes
+import sympy
 
-print("python3 rsa_tool.py n e c")
+n = int(input("n value > "))
+e = int(input("e value > "))
+c = int(input("c value > "))
 
-n = int(sys.argv[1])
-e = int(sys.argv[2])
-c = int(sys.argv[3])
-
-f = FactorDB(n)
-f.connect()
-factors = f.get_factor_list()
-p = factors[0]
-q= factors[1]
-
-phi = (p-1)*(q-1)
+if sympy.isprime(n):
+	phi = n-1
+else:
+	f = FactorDB(n)
+	f.connect()
+	factors = f.get_factor_list()
+	p = factors[0]
+	q = factors[1]
+	if p==q:
+		phi = p*(q-1)
+	else:
+		phi = (p-1)*(q-1)
+		
 d = inverse(e, phi)
 m = pow(c, d, n)
 
