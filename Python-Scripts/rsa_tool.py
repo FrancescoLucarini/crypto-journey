97391136d834bb607241f1c03e59e2475ac50efd
Francesco Lucarini (Apothiphis_Z) <69625915+FrancescoLucarini@users.noreply.github.com>
2023-06-13 21:29:52 +0200

    Update rsa_tool.py

--- Python-Scripts/rsa_tool.py
+++ Python-Scripts/rsa_tool.py
@@ -12,13 +12,20 @@ else:
 	f = FactorDB(n)
 	f.connect()
 	factors = f.get_factor_list()
-	p = factors[0]
-	q = factors[1]
-	if p==q:
-		phi = p*(q-1)
-	else:
-		phi = (p-1)*(q-1)
+	if len(factors) == 2:
+		p = factors[0]
+		q = factors[1]
+		if p==q:
+			phi = p*(q-1)
+		else:
+			phi = (p-1)*(q-1)
 		
+	else:
+		phi = 1
+		for factor in factors:
+			phi *= (factor-1)
+	
+	
 d = inverse(e, phi)
 m = pow(c, d, n)
 
